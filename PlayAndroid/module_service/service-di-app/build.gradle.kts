4f958031db579620df299b7c013145694faa7028
13471448316 <199716long>
2022-05-04 21:51:12 +0800

    增加multidex

--- PlayAndroid/module_service/service-di-app/build.gradle.kts
+++ PlayAndroid/module_service/service-di-app/build.gradle.kts
@@ -1,3 +1,5 @@
+
+
 plugins {
     id("com.android.library")
     id ("kotlin-android")
@@ -8,8 +10,10 @@ apply {
     from("../../basic_build.gradle")
 }
 
+
 dependencies {
     implementation (Hilt.implHilt)
     implementation(project(mapOf("path" to ":module_common:common-base")))
     kapt(Hilt.compilerHilt)
+    kapt("com.android.support:multidex:1.0.3")
 }
--- PlayAndroid/module_service/service-di-app/src/main/java/com/dragon/service_di_app/PlayAndroidApplication.kt
+++ PlayAndroid/module_service/service-di-app/src/main/java/com/dragon/service_di_app/PlayAndroidApplication.kt
@@ -1,7 +1,14 @@
 package com.dragon.service_di_app
 
+import android.content.Context
+
 import com.dragon.module_base.base.BaseApplication
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class PlayAndroidApplication : BaseApplication()
+class PlayAndroidApplication : BaseApplication(){
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+//        MultiDex.install(this)
+    }
+}
