9275905c51098d5f796453c3ac8c9087f8086fb1
13471448316 <199716long>
2022-08-11 19:36:59 +0800

    kt版本调整

--- PlayAndroid/build.gradle.kts
+++ PlayAndroid/build.gradle.kts
@@ -10,6 +10,5 @@ buildscript {
         classpath(libs.kotlin.serializationPlugin)
         classpath(libs.hilt.gradlePlugin)
         classpath(libs.secrets.gradlePlugin)
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
     }
 }
